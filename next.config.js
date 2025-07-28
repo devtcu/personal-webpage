49990122d426809c2bc9fda136634501c1135813
Devansh KM <143231036+devtcu@users.noreply.github.com>
2025-07-27 22:30:56 -0400

    Build for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -3,16 +3,16 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ['localhost'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
+    unoptimized: true,
     formats: ['image/webp', 'image/avif']
   },
-  output: 'standalone'
+  output: 'export',
+  // Set basePath to your repository name if not using a custom domain
+  // Use the name of your GitHub repository
+  basePath: process.env.NODE_ENV === 'production' ? '/personal-webpage' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-webpage/' : '',
+  // This ensures that GitHub Pages won't try to process files with Jekyll
+  distDir: 'out',
 };
 
 module.exports = nextConfig;
